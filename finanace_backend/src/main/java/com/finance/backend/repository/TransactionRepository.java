05a1ab29c9d6fc140d2293b924045680c504c84a
karan jha <Karanjhax12@gmail.com>
2026-04-03 13:45:54 +0530

    project setup

---
+++ finanace_backend/src/main/java/com/finance/backend/repository/TransactionRepository.java
@@ -0,0 +1,17 @@
+package com.finance.backend.repository;
+
+import com.finance.backend.enums.TransactionType;
+import com.finance.backend.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByType(TransactionType type);
+    List<Transaction> findByCategory(String category);
+    List<Transaction> findByDateBetween(LocalDate start, LocalDate end);
+    List<Transaction> findByCratedById(Long userId);
+}
